d4c38f76983e5c64b0c04b38fc253a02e0210412
him8033 <himanshu8033@gmail.com>
2025-03-04 23:00:40 +0530

    a

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,3 @@
-// const baseApi = "https://school-management-system-00wq.onrender.com/api";
-const baseApi = "https://school-management-system-seven-taupe.vercel.app/api";
+const baseApi = "https://school-management-system-00wq.onrender.com/api";
 
 export { baseApi }
